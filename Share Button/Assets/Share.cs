732c5cd076fefd8854f50122f4018d80bc219046
Jose Alberto Montero Alvarez <josealmon2012@gmail.com>
2021-04-14 12:39:42 +0200

    Done
    uwu

---
+++ Share Button/Assets/Share.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Share : MonoBehaviour
+{
+
+    public void Compartir()
+    {
+        StartCoroutine("ShareImg");
+    }
+
+    IEnumerator ShareImg()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D tx = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        tx.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        tx.Apply();
+
+        string path = Path.Combine(Application.temporaryCachePath, "sharedImage.png");
+        File.WriteAllBytes(path, tx.EncodeToPNG());
+
+        Destroy(tx); //to avoid memory leaks
+
+        new NativeShare()
+            .AddFile(path)
+            .SetSubject("uwu")
+            .SetText("uwu")
+            .Share();
+
+    }
+}
